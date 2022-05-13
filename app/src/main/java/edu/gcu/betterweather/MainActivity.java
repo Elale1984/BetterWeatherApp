65f76696bbcaf95d804d4b40c0bee7e75124dc0c
edwar <javaeddie84@gmail.com>
2022-05-13 18:51:59 -0500

    init

---
+++ app/src/main/java/edu/gcu/betterweather/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.gcu.betterweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+// Create
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
