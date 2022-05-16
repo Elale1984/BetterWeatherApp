77cfce8402b432638af40516407e9deb0052c82a
elale1984 <elale@mygcu.edu>
2022-05-16 00:55:18 -0500

    Cleaned up unused code and added button click with toast to change activity

--- app/src/main/java/edu/gcu/betterweather/BWALoginView.java
+++ app/src/main/java/edu/gcu/betterweather/BWALoginView.java
@@ -2,13 +2,32 @@ package edu.gcu.betterweather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class BWALoginView extends AppCompatActivity {
+
+    Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bwalogin_view);
+
+        btnLogin = findViewById(R.id.btnLogIn);
+
+        btnLogin.setOnClickListener(view -> {
+            Toast.makeText(BWALoginView.this, "Log In Successful", Toast.LENGTH_SHORT).show();
+            LoginSuccess();
+        });
+    }
+
+    public void LoginSuccess(){
+
+
+        Intent intent = new Intent(BWALoginView.this, MainUI.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/edu/gcu/betterweather/Repository.java
+++
@@ -1,6 +0,0 @@
-package edu.gcu.betterweather;
-
-public class Repository {
-
-
-}
