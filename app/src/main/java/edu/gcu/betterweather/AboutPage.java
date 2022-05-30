2bb94397659df797883440db59fe5fa2b087d5cb
Lindsay199904 <lindsayblood9@gmail.com>
2022-05-29 19:12:27 -0500

    Added Changes Related to About page

---
+++ app/src/main/java/edu/gcu/betterweather/AboutPage.java
@@ -0,0 +1,14 @@
+package edu.gcu.betterweather;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
